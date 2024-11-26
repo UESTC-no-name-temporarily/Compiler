6df6a0c896f84e12e415eefac15eb2db80f45229
SYH <shanyihao233@gmail.com>
2024-11-26 21:29:30 +0800

    优化 BasicBlock、Func 和 Module 类，更新指令和基本块管理方法，改进内部数据结构

--- ir/CFG.cpp
+++ ir/CFG.cpp
@@ -3,50 +3,53 @@
 #include "./include/ir/BasicClass.hpp"
 
 void BasicBlock::addInst(Inst* inst){
-    insts.push_back(inst);
+    //TODO need to be modified
+    instList.push_back(inst);
     inst->setParent(this);
 }
 void BasicBlock::deleteInst(Inst* inst){
-    auto it = std::find(insts.begin(), insts.end(), inst);
-    if (it != insts.end()) {
-        insts.erase(it);
+    auto it = std::find(instList.begin(), instList.end(), inst);
+    if (it != instList.end()) {
+        instList.erase(it);
     }
 }
 void BasicBlock::dump() const{
-    std::cout << "BasicBlock: Name = " << name << "\n";
-    for (const auto& inst : insts) {
+    std::cout << "BasicBlock: Name = " << GetName() << "\n";
+    for (const auto& inst : instList) {
         inst->dump();
     }
 }
 void Func::addBB(BasicBlock* bb){
-    bbs.push_back(bb);
+    //TODO need to be modified
+    bbList.push_back(bb);
     bb->setParent(this);
 }
 void Func::deleteBB(BasicBlock* bb){
-    auto it = std::find(bbs.begin(), bbs.end(), bb);
-    if (it != bbs.end()) {
-        bbs.erase(it);
+    auto it = std::find(bbList.begin(), bbList.end(), bb);
+    if (it != bbList.end()) {
+        bbList.erase(it);
     }
 }
 void Func::dump() const{
     std::cout << "Function:\n";
-    for (const auto& bb : bbs) {
+    for (const auto& bb : bbList) {
         bb->dump();
     }
 }
 void Module::addFunc(Func* func){
-    funcs.push_back(func);
+    //TODO need to be modified
+    funcList.push_back(func);
     func->setParent(this);
 }
 void Module::deleteFunc(Func* func){
-    auto it = std::find(funcs.begin(), funcs.end(), func);
-    if (it != funcs.end()) {
-        funcs.erase(it);
+    auto it = std::find(funcList.begin(), funcList.end(), func);
+    if (it != funcList.end()) {
+        funcList.erase(it);
     }
 }
 void Module::dump() const{
     std::cout << "Module:\n";
-    for (const auto& func : funcs) {
+    for (const auto& func : funcList) {
         func->dump();
     }
 }
