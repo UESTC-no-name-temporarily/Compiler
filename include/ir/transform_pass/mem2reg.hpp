d4a6b233905ecb50199e8eb5fece2cb7555d51f9
SYH <shanyihao233@gmail.com>
2025-02-26 13:47:51 +0800

    apply passmanager

--- include/ir/transform_pass/mem2reg.hpp
+++ include/ir/transform_pass/mem2reg.hpp
@@ -2,11 +2,17 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-
+#include "../BasicClass.hpp"
+#include "../CFG.hpp"
 class Mem2Reg {
+    Func *func;
+    Mem2Reg(Func &F) {
+        func = &F;
+    }
+    
 public:
     void run();
 private:
-    void promoteMemoryToRegister(Function &F);
+    void promoteMemoryToRegister(Func &F);
 
 };
